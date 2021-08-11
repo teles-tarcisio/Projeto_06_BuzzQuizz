07875805f9c017d83effc6b8c5a33e235f3355e0
Tarcísio F. S. Teles <tfst@poli.br / tfst@ecomp.poli.br>
2021-08-11 19:30:44 -0300

    printing quizzes to html ok, get single quiz ok

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,26 +1,36 @@
-const QUIZZES_URL = "https://mock-api.bootcamp.respondeai.com.br/api/v3/buzzquizz/quizzes";
+const QUIZZES_URL = "https://mock-api.bootcamp.respondeai.com.br/api/v3/buzzquizz/quizzes/";
 
-/*-------------------------------------------------------*/
+/*Getting all quizzes------------------------------------*/
 
-function getQuizzesFromServer() {
+function getAllQuizzes() {
     const quizzPromise = axios.get(QUIZZES_URL);
-    quizzPromise.then(gotQuizzes);
+    quizzPromise.then(printQuizzesToHTML);
     quizzPromise.catch(errorGettingQuizzes);
 }
 
-function gotQuizzes(serverResponse) {
-    console.log("got quizzes successfully: ", serverResponse.data);
-    printQuizzesToHTML(serverResponse);
-    
-}
 function errorGettingQuizzes(serverError) {
     console.log("error getting quizzes: ", serverError);
 }
 
-function printQuizzesToHTML(received) {
+function printQuizzesToHTML(response) {
     let allQuizzesList = document.querySelector(".all-quizzes ul");
-    allQuizzesList.innerHTML += `<li>
-    <div> <h2> ${received.data[0].title} </h2> </div>
-    <img src="${received.data[0].image}">
-    </li>`;
+    for (i = 0; i < response.data.length; i++) {
+        allQuizzesList.innerHTML += `<li>
+        <div> <h2> ${response.data[i].title} </h2> </div>
+        <img src="${response.data[i].image}">
+        </li>`;
+    }
+    console.log("finished printing quizzes to html");
 }
+
+/*Getting a single quiz------------------------------------*/
+
+function getSingleQuiz(quizID) {
+    const quizzPromise = axios.get(`${QUIZZES_URL}` + quizID);
+    quizzPromise.then(console.log);
+    quizzPromise.catch(console.log);
+}
+
+
+
+/*Submitting a quiz--------------------------------------*/
