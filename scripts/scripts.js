d89549b9b805ebf956a2bda587d128fb932ff776
Tarcísio F. S. Teles <tfst@poli.br / tfst@ecomp.poli.br>
2021-08-11 19:03:12 -0300

    getting quiz object from server and adding to all-quizzes_ul ok, WIP

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,15 +3,24 @@ const QUIZZES_URL = "https://mock-api.bootcamp.respondeai.com.br/api/v3/buzzquiz
 /*-------------------------------------------------------*/
 
 function getQuizzesFromServer() {
-    const quizzPromise = axios.getQuizzesFromServer(QUIZZES_URL);
+    const quizzPromise = axios.get(QUIZZES_URL);
     quizzPromise.then(gotQuizzes);
     quizzPromise.catch(errorGettingQuizzes);
 }
 
 function gotQuizzes(serverResponse) {
-    console.log("got quizzes successfully: ", serverResponse);
+    console.log("got quizzes successfully: ", serverResponse.data);
+    printQuizzesToHTML(serverResponse);
+    
 }
-
 function errorGettingQuizzes(serverError) {
     console.log("error getting quizzes: ", serverError);
+}
+
+function printQuizzesToHTML(received) {
+    let allQuizzesList = document.querySelector(".all-quizzes ul");
+    allQuizzesList.innerHTML += `<li>
+    <div> <h2> ${received.data[0].title} </h2> </div>
+    <img src="${received.data[0].image}">
+    </li>`;
 }
