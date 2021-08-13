71a1700e8a88ec19263a065bf66fb3729dcbfa07
jefranca <jean13franca@gmail.com>
2021-08-12 21:38:52 -0300

    preparing the create quiz system

--- scripts/scripts03.js
+++ scripts/scripts03.js
@@ -1,1 +1,89 @@
-function open
+// esquema da variavel para enviar no post
+//let CreatedQuiz = {
+//     title,
+//     image,
+//     questions:{
+//         title,
+//         color,
+//         answer: {
+//             title,
+//             image,
+//             isCorrectAnswer
+//         }
+//     },
+//     levels:{
+//         title,
+//         image, 
+//         text,
+//         minValue
+//     }
+
+// }
+
+function createQuizPage(){
+    let mainDiv=document.querySelector("main");
+    mainDiv.innerHTML =`<div class="new-quiz first-page">
+                            <h1>
+                                Comece pelo começo
+                            </h1>
+                            <div>
+                            <input type="text" placeholder="Título do seu quizz">
+                            <input type="text" placeholder="URL da imagem do seu quizz">
+                            <input type="text" placeholder="Quantidade de perguntas do quizz">
+                            <input type="text" placeholder="Quantidade de níveis do quizz">
+                            </div>
+                            <button onclick="showSecondPage()"> 
+                                Prosseguir para criar perguntas
+                            </button>
+                        </div>`;
+} 
+// funcao para salvar dados na variavel antes de envia-la(rascunho)
+//function saveInfoFirstPage(){
+//     title:<input type="text" placeholder="Título do seu quizz">.value
+//     image:<input type="text" placeholder="URL da imagem do seu quizz">.value
+//     questions.length:<input type="text" placeholder="Quantidade de perguntas do quizz">.value
+//     levels.length:<input type="text" placeholder="Quantidade de níveis do quizz">.value
+// }
+function showSecondPage(){
+//    saveInfoFirstPage;
+    let quizpage = document.querySelector(".new-quiz");
+    quizpage.classList.add("second-page");
+    quizpage.classList.remove("first-page");
+    quizpage.innerHTML = "";
+    quizpage.innerHTML += `<h1>
+                                Crie suas perguntas
+                            </h1>
+                            <div>
+                                <h2>Pergunta 1</h2>
+                                <input type="text" placeholder="Texto da pergunta">
+                                <input type="text" placeholder="Cor de fundo da pergunta">
+                                <h2>Resposta correta</h2>
+                                <input type="text" placeholder="Resposta correta">
+                                <input type="text" placeholder="URL da imagem">
+                                <h2>Resposta incorretas</h2>
+                                <input type="text" placeholder="Resposta incorreta 1">
+                                <input type="text" placeholder="URL da imagem 1">
+                                <input type="text" placeholder="Resposta incorreta 2">
+                                <input type="text" placeholder="URL da imagem 2">
+                                <input type="text" placeholder="Resposta incorreta 3">
+                                <input type="text" placeholder="URL da imagem 3">
+                            </div>
+                            <div>
+                                <h2>Pergunta 2</h2>
+                                <input type="text" placeholder="Texto da pergunta">
+                                <input type="text" placeholder="Cor de fundo da pergunta">
+                                <h2>Resposta correta</h2>
+                                <input type="text" placeholder="Resposta correta">
+                                <input type="text" placeholder="URL da imagem">
+                                <h2>Resposta incorretas</h2>
+                                <input type="text" placeholder="Resposta incorreta 1">
+                                <input type="text" placeholder="URL da imagem 1">
+                                <input type="text" placeholder="Resposta incorreta 2">
+                                <input type="text" placeholder="URL da imagem 2">
+                                <input type="text" placeholder="Resposta incorreta 3">
+                                <input type="text" placeholder="URL da imagem 3">
+                            </div>
+                            <button>
+                                Prosseguir para criar níveis
+</button>`
+}
