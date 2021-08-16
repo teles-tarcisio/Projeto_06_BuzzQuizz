9aa81536cd1c2cb2c0524fa9905ff86a0e6d110e
Tarcísio F. S. Teles <tfst@poli.br / tfst@ecomp.poli.br>
2021-08-16 19:45:59 -0300

    relocating scripts from pages 01 and 02

--- scripts/scripts-screen01-02.js
+++ scripts/scripts-screen01-02.js
@@ -1,3 +1,33 @@
+const QUIZZES_URL = "https://mock-api.bootcamp.respondeai.com.br/api/v3/buzzquizz/quizzes/";
+
+function getAllQuizzes() {
+    const quizzPromise = axios.get(QUIZZES_URL);
+    quizzPromise.then(printQuizzesToHTML);
+    quizzPromise.catch(errorGettingQuizzes);
+}
+
+function errorGettingQuizzes(serverError) {
+    alert("Error fetching quizzes from server!");
+    console.log("error getting quizzes: ", serverError);
+}
+
+const allQuizzesArray = [];
+function printQuizzesToHTML(serverResponse) {
+    for (i = 0; i < serverResponse.data.length; i++) {
+        allQuizzesArray.push(serverResponse.data[i]);        
+    };
+    
+    let allQuizzesList = document.querySelector(".all-quizzes ul");
+    for (i = 0; i < allQuizzesArray.length; i++) {
+        allQuizzesList.innerHTML += `<li onclick="getSelectedQuizz(this)">
+        <div id="${allQuizzesArray[i].id}"> <h2> ${allQuizzesArray[i].title} </h2> </div>
+        <img src="${allQuizzesArray[i].image}">
+        </li>`;
+    }
+}
+///////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////
+
 // GLOBAL VARIABLE ----------------------------------------
 // object that replicates a single quizz sent by server:
 let localQuizzReplica = {
@@ -166,3 +196,5 @@ function insertQuizzResult() {
         </div>`;
     return insertResult;
 }
+
+
